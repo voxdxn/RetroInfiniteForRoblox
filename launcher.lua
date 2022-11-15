7c39beb436fbd8829a6fdd37ef0231ee100e50a5
voxdxn <112600442+voxdxn@users.noreply.github.com>
2022-11-15 16:39:29 -0500

    Create launcher.lua

---
+++ launcher.lua
@@ -0,0 +1,18 @@
+[ loading ]
+   
+_G.Setting = {
+  KeySystem = true
+  KeyActivate = true
+  
+  end
+end
+
+_G.Setting = {
+    StopRender = false, 
+    ReduceBug = true, 
+    ReduceLag = true, 
+    NoDamage = true, 
+    WalkSpeed = true,
+    NoClip = true
+    NoNT = true
+}
